d93a7b9f067fc14ff0401243070e7a60e7f6c8aa
Yusei-YK <sojawike15@gmail.com>
2026-07-06 12:49:49 +0800

    整理：移出历史工程并清理 git 索引

--- Abs_Encoder_SPI_264Demo/Debug/Libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPsi5_cfg.d
+++
@@ -1,4 +0,0 @@
-IfxPsi5_cfg.o :	../Libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPsi5_cfg.c
-../Libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxPsi5_cfg.c :
-IfxPsi5_cfg.o :	..\Libraries\infineon_libraries\iLLD\TC26B\Tricore\_Impl\IfxPsi5_cfg.h
-..\Libraries\infineon_libraries\iLLD\TC26B\Tricore\_Impl\IfxPsi5_cfg.h :
--- Abs_Encoder_SPI_264Demo/Debug/Libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxSrc_cfg.d
+++
@@ -1,4 +0,0 @@
-IfxSrc_cfg.o :	../Libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxSrc_cfg.c
-../Libraries/infineon_libraries/iLLD/TC26B/Tricore/_Impl/IfxSrc_cfg.c :
-IfxSrc_cfg.o :	..\Libraries\infineon_libraries\iLLD\TC26B\Tricore\_Impl\IfxSrc_cfg.h
-..\Libraries\infineon_libraries\iLLD\TC26B\Tricore\_Impl\IfxSrc_cfg.h :
