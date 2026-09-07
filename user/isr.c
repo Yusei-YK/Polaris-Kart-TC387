252688ce699f297d0efa313457ede2141bad5812
Yusei-YK <sojawike15@gmail.com>
2026-09-07 12:09:57 +0800

    fix(isr):isr.c 里 1073 个乱码字符复原,全是逐飞原注释
    以前判断"能 utf-8 解码就没坏"是错的:U+FFFD 本身就是合法 UTF-8 序列,
    所以坏文件照样解得开。改用字节计数 EF BF BD 才查得出来,isr.c 是全仓最严重的
    一个,85 行受损。
    
    词源都在仓库里:
      抬头 24 行取自 user/isr.h,那份是同一段样板且零乱码,文件名/ADS 版本/修改
        记录三个字段本来就和 isr.c 一致,整段可搬。
      正文 61 行取自 055f328:Kart_TC387/user/isr.c,那时还是 GBK,没坏。按 ASCII
        骨架单调对齐,61 行全部唯一命中。
    
    只动注释:脚本校验了去注释后代码逐字节相同、行数与 CRLF 不变、每行替换前后
    ASCII 骨架相同、花括号与预处理行数不变。改完 U+FFFD 为 0。

--- user/isr.c
+++ user/isr.c
@@ -1,35 +1,35 @@
 /*********************************************************************************************************************
-* Kart_TC387 Opensourec Library ����Kart_TC387 ��Դ�⣩��һ�����ڹٷ� SDK �ӿڵĵ�������Դ��
-* Copyright (c) 2022 SEEKFREE ��ɿƼ�
+* Kart_TC387 Opensourec Library 即（Kart_TC387 开源库）是一个基于官方 SDK 接口的第三方开源库
+* Copyright (c) 2022 SEEKFREE 逐飞科技
 *
-* ���ļ��� Kart_TC387 ��Դ���һ����
+* 本文件是 Kart_TC387 开源库的一部分
 *
-* Kart_TC387 ��Դ�� ���������
-* �����Ը���������������ᷢ���� GPL��GNU General Public License���� GNUͨ�ù�������֤��������
-* �� GPL �ĵ�3�棨�� GPL3.0������ѡ��ģ��κκ����İ汾�����·�����/���޸���
+* Kart_TC387 开源库 是免费软件
+* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
 *
-* ����Դ��ķ�����ϣ�����ܷ������ã�����δ�������κεı�֤
-* ����û�������������Ի��ʺ��ض���;�ı�֤
-* ����ϸ����μ� GPL
+* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+* 甚至没有隐含的适销性或适合特定用途的保证
+* 更多细节请参见 GPL
 *
-* ��Ӧ�����յ�����Դ���ͬʱ�յ�һ�� GPL �ĸ���
-* ���û�У������<https://www.gnu.org/licenses/>
+* 您应该在收到本开源库的同时收到一份 GPL 的副本
+* 如果没有，请参阅<https://www.gnu.org/licenses/>
 *
-* ����ע����
-* ����Դ��ʹ�� GPL3.0 ��Դ����֤Э�� ������������Ϊ���İ汾
-* ��������Ӣ�İ��� libraries/doc �ļ����µ� GPL3_permission_statement.txt �ļ���
-* ����֤������ libraries �ļ����� �����ļ����µ� LICENSE �ļ�
-* ��ӭ��λʹ�ò����������� ���޸�����ʱ���뱣����ɿƼ��İ�Ȩ����������������
+* 额外注明：
+* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
 *
-* �ļ�����          isr
-* ��˾����          �ɶ���ɿƼ����޹�˾
-* �汾��Ϣ          �鿴 libraries/doc �ļ����� version �ļ� �汾˵��
-* ��������          ADS v1.10.2
-* ����ƽ̨          TC387QP
-* ��������          https://seekfree.taobao.com/
+* 文件名称          isr
+* 公司名称          成都逐飞科技有限公司
+* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+* 开发环境          ADS v1.10.2
+* 适用平台          TC387QP
+* 店铺链接          https://seekfree.taobao.com/
 *
-* �޸ļ�¼
-* ����              ����                ��ע
+* 修改记录
+* 日期              作者                备注
 * 2022-11-04       pudding            first version
 ********************************************************************************************************************/
 #include "isr_config.h"
@@ -63,10 +63,10 @@ volatile uint32 g_tick_5ms = 0;
  * 手动接进 main 就能直接用。要看它,现在得自己去调那个自检函数。 */
 volatile uint32 g_dot_sync_edges = 0;
 
-// ����TCϵ��Ĭ���ǲ�֧���ж�Ƕ�׵ģ�ϣ��֧���ж�Ƕ����Ҫ���ж���ʹ�� interrupt_global_enable(0); �������ж�Ƕ��
-// �򵥵�˵ʵ���Ͻ����жϺ�TCϵ�е�Ӳ���Զ������� interrupt_global_disable(); ���ܾ���Ӧ�κε��жϣ������Ҫ�����Լ��ֶ����� interrupt_global_enable(0); �������жϵ���Ӧ��
+// 对于TC系列默认是不支持中断嵌套的，希望支持中断嵌套需要在中断内使用 interrupt_global_enable(0); 来开启中断嵌套
+// 简单点说实际上进入中断后TC系列的硬件自动调用了 interrupt_global_disable(); 来拒绝响应任何的中断，因此需要我们自己手动调用 interrupt_global_enable(0); 来开启中断的响应。
 
-// **************************** PIT�жϺ��� ****************************
+// **************************** PIT中断函数 ****************************
 IFX_INTERRUPT(cc60_pit_ch0_isr, CCU6_0_CH0_INT_VECTAB_NUM, CCU6_0_CH0_ISR_PRIORITY)
 {
     interrupt_global_enable(0);
@@ -116,7 +116,7 @@ IFX_INTERRUPT(cc61_pit_ch0_isr, CCU6_1_CH0_INT_VECTAB_NUM, CCU6_1_CH0_ISR_PRIORI
 
 IFX_INTERRUPT(cc61_pit_ch1_isr, CCU6_1_CH1_INT_VECTAB_NUM, CCU6_1_CH1_ISR_PRIORITY)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     pit_clear_flag(CCU61_CH1);
 
 
@@ -124,14 +124,14 @@ IFX_INTERRUPT(cc61_pit_ch1_isr, CCU6_1_CH1_INT_VECTAB_NUM, CCU6_1_CH1_ISR_PRIORI
 
 
 }
-// **************************** PIT�жϺ��� ****************************
+// **************************** PIT中断函数 ****************************
 
 
-// **************************** �ⲿ�жϺ��� ****************************
+// **************************** 外部中断函数 ****************************
 IFX_INTERRUPT(exti_ch0_ch4_isr, EXTI_CH0_CH4_INT_VECTAB_NUM, EXTI_CH0_CH4_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
-    if(exti_flag_get(ERU_CH0_REQ0_P15_4))           // ͨ��0�ж�
+    interrupt_global_enable(0);                     // 开启中断嵌套
+    if(exti_flag_get(ERU_CH0_REQ0_P15_4))           // 通道0中断
     {
         exti_flag_clear(ERU_CH0_REQ0_P15_4);
 
@@ -150,13 +150,13 @@ IFX_INTERRUPT(exti_ch0_ch4_isr, EXTI_CH0_CH4_INT_VECTAB_NUM, EXTI_CH0_CH4_INT_PR
 
 IFX_INTERRUPT(exti_ch1_ch5_isr, EXTI_CH1_CH5_INT_VECTAB_NUM, EXTI_CH1_CH5_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
-    if(exti_flag_get(ERU_CH1_REQ10_P14_3))          // ͨ��1�ж�
+    if(exti_flag_get(ERU_CH1_REQ10_P14_3))          // 通道1中断
     {
         exti_flag_clear(ERU_CH1_REQ10_P14_3);
 
-        tof_module_exti_handler();                  // ToF ģ�� INT �����ж�
+        tof_module_exti_handler();                  // ToF 模块 INT 更新中断
 
     }
 
@@ -180,15 +180,15 @@ IFX_INTERRUPT(exti_ch1_ch5_isr, EXTI_CH1_CH5_INT_VECTAB_NUM, EXTI_CH1_CH5_INT_PR
     }
 }
 
-// ��������ͷpclk����Ĭ��ռ���� 2ͨ�������ڴ���DMA��������ﲻ�ٶ����жϺ���
+// 由于摄像头pclk引脚默认占用了 2通道，用于触发DMA，因此这里不再定义中断函数
 // IFX_INTERRUPT(exti_ch2_ch6_isr, EXTI_CH2_CH6_INT_VECTAB_NUM, EXTI_CH2_CH6_INT_PRIO)
 // {
-//  interrupt_global_enable(0);                     // �����ж�Ƕ��
-//  if(exti_flag_get(ERU_CH2_REQ7_P00_4))           // ͨ��2�ж�
+//  interrupt_global_enable(0);                     // 开启中断嵌套
+//  if(exti_flag_get(ERU_CH2_REQ7_P00_4))           // 通道2中断
 //  {
 //      exti_flag_clear(ERU_CH2_REQ7_P00_4);
 //  }
-//  if(exti_flag_get(ERU_CH6_REQ9_P20_0))           // ͨ��6�ж�
+//  if(exti_flag_get(ERU_CH6_REQ9_P20_0))           // 通道6中断
 //  {
 //      exti_flag_clear(ERU_CH6_REQ9_P20_0);
 //  }
@@ -196,11 +196,11 @@ IFX_INTERRUPT(exti_ch1_ch5_isr, EXTI_CH1_CH5_INT_VECTAB_NUM, EXTI_CH1_CH5_INT_PR
 
 IFX_INTERRUPT(exti_ch3_ch7_isr, EXTI_CH3_CH7_INT_VECTAB_NUM, EXTI_CH3_CH7_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
-    if(exti_flag_get(ERU_CH3_REQ6_P02_0))           // ͨ��3�ж�
+    interrupt_global_enable(0);                     // 开启中断嵌套
+    if(exti_flag_get(ERU_CH3_REQ6_P02_0))           // 通道3中断
     {
         exti_flag_clear(ERU_CH3_REQ6_P02_0);
-        camera_vsync_handler();                     // ����ͷ�����ɼ�ͳһ�ص�����
+        camera_vsync_handler();                     // 摄像头触发采集统一回调函数
     }
     if(exti_flag_get(ERU_CH7_REQ16_P15_1))
     {
@@ -208,23 +208,23 @@ IFX_INTERRUPT(exti_ch3_ch7_isr, EXTI_CH3_CH7_INT_VECTAB_NUM, EXTI_CH3_CH7_INT_PR
 
     }
 }
-// **************************** �ⲿ�жϺ��� ****************************
+// **************************** 外部中断函数 ****************************
 
 
-// **************************** DMA�жϺ��� ****************************
+// **************************** DMA中断函数 ****************************
 IFX_INTERRUPT(dma_ch5_isr, DMA_INT_VECTAB_NUM, DMA_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
-    camera_dma_handler();                           // ����ͷ�ɼ����ͳһ�ص�����
+    interrupt_global_enable(0);                     // 开启中断嵌套
+    camera_dma_handler();                           // 摄像头采集完成统一回调函数
 }
-// **************************** DMA�жϺ��� ****************************
+// **************************** DMA中断函数 ****************************
 
 
-// **************************** �����жϺ��� ****************************
-// ����0Ĭ����Ϊ���Դ���
+// **************************** 串口中断函数 ****************************
+// 串口0默认作为调试串口
 IFX_INTERRUPT(uart0_tx_isr, UART0_INT_VECTAB_NUM, UART0_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -243,10 +243,10 @@ IFX_INTERRUPT(uart0_rx_isr, UART0_INT_VECTAB_NUM, UART0_RX_INT_PRIO)
 }
 
 
-// ����1Ĭ�����ӵ�����ͷ���ô���
+// 串口1默认连接到摄像头配置串口
 IFX_INTERRUPT(uart1_tx_isr, UART1_INT_VECTAB_NUM, UART1_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -275,10 +275,10 @@ IFX_INTERRUPT(uart1_rx_isr, UART1_INT_VECTAB_NUM, UART1_RX_INT_PRIO)
 #endif
 }
 
-// ����2Ĭ�����ӵ�����ת����ģ��
+// 串口2默认连接到无线转串口模块
 IFX_INTERRUPT(uart2_tx_isr, UART2_INT_VECTAB_NUM, UART2_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -286,7 +286,7 @@ IFX_INTERRUPT(uart2_tx_isr, UART2_INT_VECTAB_NUM, UART2_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart2_rx_isr, UART2_INT_VECTAB_NUM, UART2_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     /* CH32 油门/刹车踏板盒收字节(UART_2 = ASCLIN2,RX = P02.1)。
      * 原先这里挂的是逐飞例程的 wireless_module_uart_handler():本车的无线
      * 模块走 SPI(见 kart_wifi.c),那个回调没有对应硬件,是模板残留。 */
@@ -295,10 +295,10 @@ IFX_INTERRUPT(uart2_rx_isr, UART2_INT_VECTAB_NUM, UART2_RX_INT_PRIO)
 
 
 }
-// ����3Ĭ�����ӵ�GPS��λģ��
+// 串口3默认连接到GPS定位模块
 IFX_INTERRUPT(uart3_tx_isr, UART3_INT_VECTAB_NUM, UART3_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -306,14 +306,14 @@ IFX_INTERRUPT(uart3_tx_isr, UART3_INT_VECTAB_NUM, UART3_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart3_rx_isr, UART3_INT_VECTAB_NUM, UART3_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     kart_remote_rx_callback();                      // SBUS 遥控接收:逐字节攒帧解析(UART3,P15.7 RX)
 }
 
 
 IFX_INTERRUPT(uart4_tx_isr, UART4_INT_VECTAB_NUM, UART4_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -321,7 +321,7 @@ IFX_INTERRUPT(uart4_tx_isr, UART4_INT_VECTAB_NUM, UART4_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart4_rx_isr, UART4_INT_VECTAB_NUM, UART4_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -329,7 +329,7 @@ IFX_INTERRUPT(uart4_rx_isr, UART4_INT_VECTAB_NUM, UART4_RX_INT_PRIO)
 
 IFX_INTERRUPT(uart5_tx_isr, UART5_INT_VECTAB_NUM, UART5_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -337,7 +337,7 @@ IFX_INTERRUPT(uart5_tx_isr, UART5_INT_VECTAB_NUM, UART5_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart5_rx_isr, UART5_INT_VECTAB_NUM, UART5_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -345,7 +345,7 @@ IFX_INTERRUPT(uart5_rx_isr, UART5_INT_VECTAB_NUM, UART5_RX_INT_PRIO)
 
 IFX_INTERRUPT(uart6_tx_isr, UART6_INT_VECTAB_NUM, UART6_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -353,7 +353,7 @@ IFX_INTERRUPT(uart6_tx_isr, UART6_INT_VECTAB_NUM, UART6_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart6_rx_isr, UART6_INT_VECTAB_NUM, UART6_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -361,7 +361,7 @@ IFX_INTERRUPT(uart6_rx_isr, UART6_INT_VECTAB_NUM, UART6_RX_INT_PRIO)
 
 IFX_INTERRUPT(uart8_tx_isr, UART8_INT_VECTAB_NUM, UART8_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -369,7 +369,7 @@ IFX_INTERRUPT(uart8_tx_isr, UART8_INT_VECTAB_NUM, UART8_TX_INT_PRIO)
 
 //IFX_INTERRUPT(uart8_rx_isr, UART8_INT_VECTAB_NUM, UART8_RX_INT_PRIO)
 //{
-//    interrupt_global_enable(0);                     // �����ж�Ƕ��
+//    interrupt_global_enable(0);                     // 开启中断嵌套
 //
 //
 //
@@ -377,7 +377,7 @@ IFX_INTERRUPT(uart8_tx_isr, UART8_INT_VECTAB_NUM, UART8_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart9_tx_isr, UART9_INT_VECTAB_NUM, UART9_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -385,7 +385,7 @@ IFX_INTERRUPT(uart9_tx_isr, UART9_INT_VECTAB_NUM, UART9_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart9_rx_isr, UART9_INT_VECTAB_NUM, UART9_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -393,7 +393,7 @@ IFX_INTERRUPT(uart9_rx_isr, UART9_INT_VECTAB_NUM, UART9_RX_INT_PRIO)
 
 IFX_INTERRUPT(uart10_tx_isr, UART10_INT_VECTAB_NUM, UART10_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -401,7 +401,7 @@ IFX_INTERRUPT(uart10_tx_isr, UART10_INT_VECTAB_NUM, UART10_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart10_rx_isr, UART10_INT_VECTAB_NUM, UART10_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
     /* 语音模块不在这里：kart_voice_poll() 用 uart_query_byte() 直读硬件 RX FIFO，
      * 它的帧很稀疏（人说一句才来一帧），轮询足够。
@@ -422,7 +422,7 @@ IFX_INTERRUPT(uart10_rx_isr, UART10_INT_VECTAB_NUM, UART10_RX_INT_PRIO)
 
 IFX_INTERRUPT(uart11_tx_isr, UART11_INT_VECTAB_NUM, UART11_TX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
@@ -430,65 +430,65 @@ IFX_INTERRUPT(uart11_tx_isr, UART11_INT_VECTAB_NUM, UART11_TX_INT_PRIO)
 
 IFX_INTERRUPT(uart11_rx_isr, UART11_INT_VECTAB_NUM, UART11_RX_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
 
 
 
 }
-// ����ͨѶ�����ж�
+// 串口通讯错误中断
 IFX_INTERRUPT(uart0_er_isr, UART0_INT_VECTAB_NUM, UART0_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart0_handle);
 }
 IFX_INTERRUPT(uart1_er_isr, UART1_INT_VECTAB_NUM, UART1_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart1_handle);
 }
 IFX_INTERRUPT(uart2_er_isr, UART2_INT_VECTAB_NUM, UART2_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart2_handle);
 }
 IFX_INTERRUPT(uart3_er_isr, UART3_INT_VECTAB_NUM, UART3_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart3_handle);
 }
 IFX_INTERRUPT(uart4_er_isr, UART4_INT_VECTAB_NUM, UART4_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart4_handle);
 }
 IFX_INTERRUPT(uart5_er_isr, UART5_INT_VECTAB_NUM, UART5_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart5_handle);
 }
 IFX_INTERRUPT(uart6_er_isr, UART6_INT_VECTAB_NUM, UART6_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart6_handle);
 }
 IFX_INTERRUPT(uart8_er_isr, UART8_INT_VECTAB_NUM, UART8_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart8_handle);
 }
 IFX_INTERRUPT(uart9_er_isr, UART9_INT_VECTAB_NUM, UART9_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart9_handle);
 }
 IFX_INTERRUPT(uart10_er_isr, UART10_INT_VECTAB_NUM, UART10_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart10_handle);
 }
 IFX_INTERRUPT(uart11_er_isr, UART11_INT_VECTAB_NUM, UART11_ER_INT_PRIO)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     IfxAsclin_Asc_isrError(&uart11_handle);
 }
-// **************************** �����жϺ��� ****************************
+// **************************** 串口中断函数 ****************************
